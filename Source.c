e8d885ee0e44bf02ff82247966255097383001d2
Greenfire1234 <67749145+Greenfire1234@users.noreply.github.com>
2021-09-15 10:36:01 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+void main()
+{
+	int a;
+	long int b;
+	char c;
+	unsigned int d;
+	short e;
+	unsigned short f;
+	unsigned char h;
+	double r;
+	long double o;
+	float p;
+	unsigned long q;
+	a = -1;
+	b = 2;
+	c = 3;
+	d = 4;
+	e = -5;
+	f = 6;
+	h = 8;
+	r = 9.5;
+	o = 2020040;
+	p = -4040201;
+	q = 0;
+	
+}
